9853b9e7b7e027ff10bf823911e530f06df6e1d8
camilo <camilo.anzola@aplicaciones.com.co>
2019-08-12 10:20:46 -0500

    ok 2

--- Inner/app/src/main/java/com/example/inner/data/model/LoggedInUser.kt
+++
@@ -1,9 +0,0 @@
-package com.example.inner.data.model
-
-/**
- * Data class that captures user information for logged in users retrieved from LoginRepository
- */
-data class LoggedInUser(
-    val userId: String,
-    val displayName: String
-)
